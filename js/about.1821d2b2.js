0cdc45a992bf8944ae00e8b812c91e9e91889672
haru5386 <snsn550@gmail.com>
2022-03-08 11:41:35 +0800

    deploy

---
+++ js/about.1821d2b2.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(n,t,a){"use strict";a.r(t);var e=a("7a23"),c={class:"about"},o=Object(e["g"])("h1",null,"This is an about page",-1),s=[o];function u(n,t){return Object(e["r"])(),Object(e["f"])("div",c,s)}var r=a("d959"),b=a.n(r);const i={},d=b()(i,[["render",u]]);t["default"]=d}}]);
+//# sourceMappingURL=about.1821d2b2.js.map
